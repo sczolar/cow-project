0fea3e1d9e658779ef121a45711269349523d65f
hatz <hatz@localhost.localdomain>
2022-05-07 20:36:33 +0530

    work started

---
+++ pages/api/token.js
@@ -0,0 +1,19 @@
+
+import users from "../../db/users.json"
+
+
+export default function handler(req, res) {
+    const { token } = JSON.parse(req.body)
+    if (token) {
+        const user = users.users.map(a => (a.token === token ? a : 0))
+        if (user[0]) {
+            res.status(200).json({ message: "user alive", data: user, success: true })
+        } else {
+            res.status(400).json({ message: "user not found", success: false })
+        }
+    }
+    else {
+        res.status(400).json({ message: "token not found", success: false })
+    }
+
+}
---
+++ pages/login.js
@@ -0,0 +1,79 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { useState } from "react"
+import { useRouter } from "next/router"
+import Head from 'next/head'
+
+export default function Login() {
+    const router = useRouter();
+    const [Page, setpage] = useState({ loading: false, Message: "" })
+    const [login, setlogin] = useState({ username: "", password: "" })
+
+    //onchange handler for the value change
+    const loginhandler = (e) => {
+        setlogin({ ...login, [e.target.name]: e.target.value })
+    }
+    //while submit the value what would happen
+    const submithandler = async () => {
+        const { username, password } = login
+        if (username && password) {
+            try {
+                setpage({ ...Page, loading: true })
+                const user = await fetch("api/login", {
+                    method: "POST",
+                    headers: {
+                        contentType: "application/json",
+                    },
+                    body: JSON.stringify({ username, password })
+                }).then(res => {
+                    return res.json();
+                })
+                if (user.success) {
+                    localStorage.setItem("token", user.data[0].token)
+                    router.push("/")
+                }
+                else {
+                    setpage({ ...Page, loading: false, Message: user.message })
+                }
+            }
+            catch (err) {
+                setpage({ ...Page, loading: false, Message: err })
+            }
+        }
+        else {
+            setpage({ ...Page, Message: "all field required" })
+        }
+    }
+    if (Page.loading) { return <h1>loading....</h1> }
+    return (
+        <div className="login">
+            <Head>
+                <title>Login</title>
+                <meta name="description" content="login page" />
+            </Head>
+            <span>{Page.Message}</span>
+            <div className="login-body">
+                <h1>login</h1>
+                <form>
+                    <TextField
+                        label="Username"
+                        variant="standard"
+                        name='username'
+                        onChange={loginhandler}
+                        value={login.username}
+                        type="text"
+                    />
+                    <TextField
+                        label="Password"
+                        variant="standard"
+                        name='password'
+                        onChange={loginhandler}
+                        value={login.password}
+                        type="password"
+                    />
+                    <Button variant="contained" onClick={submithandler}>Login</Button>
+                </form>
+            </div>
+        </div>
+    )
+}
